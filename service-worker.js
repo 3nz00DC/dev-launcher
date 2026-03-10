1dc9caf1519f71f309460e8d6326e8a1de2d0e6e
3nz00DC <200856207+3nz00DC@users.noreply.github.com>
2026-03-10 12:45:29 -0400

    feat: add PWA support with manifest.json and service worker

---
+++ service-worker.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = 'dev-terminal-cache-v1';
+const urlsToCache = [
+  '/dev-launcher/',
+  '/dev-launcher/index.html',
+  '/dev-launcher/style.css',
+  '/dev-launcher/script.js',
+  '/dev-launcher/manifest.json',
+  '/dev-launcher/icons/icon-192x192.png',
+  '/dev-launcher/icons/icon-512x512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
